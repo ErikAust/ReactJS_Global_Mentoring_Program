7815b545b90f30c87aa0c0a89183676dc2873fc6
Erik <erikaust@MacBook-Air-Erik.local>
2022-05-05 17:11:21 +0300

    Core concepts complited

---
+++ my-app/src/components/FunctionalComponent.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const FunctionalComponent = () => <h1>Functional Component</h1>;
---
+++ my-app/src/components/ReactPureComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class ReactPureComponent extends React.PureComponent {
+  render () {
+    return <h1>React Component</h1>
+  }
+}
+
+export default ReactPureComponent;
